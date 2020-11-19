8110027fb71e7e6f13a3e1ef310b712f2fe86baf
Harland David Sanders <ragemp.pro@gmail.com>
2020-11-19 04:22:07 +0300

    Фикс ошибки при рестарте сервера / Fix errors during server restart

--- server/dotnet/resources/client/Core/Admin.cs
+++ server/dotnet/resources/client/Core/Admin.cs
@@ -83,7 +83,7 @@ namespace NeptuneEvo.Core
 
             Log.Write("Force kicking players...", nLog.Type.Warn);
             foreach (Player player in NAPI.Pools.GetAllPlayers())
-                NAPI.Player.KickPlayer(player, reason);
+                NAPI.Task.Run(() => NAPI.Player.KickPlayer(player, reason));
             Log.Write("All players has kicked!", nLog.Type.Success);
 
             NAPI.Task.Run(() =>
