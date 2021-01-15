bd180329cd069397f434a9fb51d3b0b541dc7c2d
Harland David Sanders <ragemp.pro@gmail.com>
2021-01-15 17:37:17 +0300

    release 2.4.1-1050

--- server/dotnet/resources/client/Main.cs
+++ server/dotnet/resources/client/Main.cs
@@ -26,8 +26,8 @@ namespace NeptuneEvo
     public class Main : Script
     { 
         public static string Codename { get; } = "redage_v2";
-        public static string Version { get; } = "2.4";
-        public static string Build { get; } = "1000";
+        public static string Version { get; } = "2.4.1";
+        public static string Build { get; } = "1050";
         // // // //
         public static string Full { get; } = $"{Codename} {Version} {Build}";
         public static DateTime StartDate { get; } = DateTime.Now;
