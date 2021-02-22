14e8a360ac3966682241623ea85e734845728988
user <user@localhost>
2021-02-23 01:49:23 +0300

    Доработка нового дизайна выбора спавна (спасибо NiceOne за тесты).

--- server/client_packages/cef/js/respawn.js
+++ server/client_packages/cef/js/respawn.js
@@ -4,11 +4,15 @@
     if (data[2] !== true) {
 		$('#home-spawn').addClass('disabled');
 		$('#home-spawn button').prop("disabled", true);
+
+    $("#home-spawn .desc-text").text('Недоступно');
 	}
 	
     if (data[1] !== true){
 		$('#org-spawn').addClass('disabled');
 		$('#org-spawn button').prop("disabled", true);
+
+    $("#org-spawn .desc-text").text('Недоступно');
     } 
 }
 
