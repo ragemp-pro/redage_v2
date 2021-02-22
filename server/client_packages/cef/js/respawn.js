ef30e393baa584a4f2136a287043561ac5a24c8a
user <user@localhost>
2021-02-22 22:53:12 +0300

    Доработка нового дизайна выбора спавна.

--- server/client_packages/cef/js/respawn.js
+++ server/client_packages/cef/js/respawn.js
@@ -1,12 +1,12 @@
 ﻿function set(data) {
     data = JSON.parse(data);
 	
-    if (data[1] !== true) {
+    if (data[2] !== true) {
 		$('#home-spawn').addClass('disabled');
 		$('#home-spawn button').prop("disabled", true);
 	}
 	
-    if (data[2] !== true){
+    if (data[1] !== true){
 		$('#org-spawn').addClass('disabled');
 		$('#org-spawn button').prop("disabled", true);
     } 
