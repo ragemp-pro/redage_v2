da704048ed9d626a06b4752f306dd67a95cd2bf9
user <user@localhost>
2021-03-01 21:32:01 +0300

    Фикс админского бессмертия.

--- server/dotnet/resources/client/Core/Commands.cs
+++ server/dotnet/resources/client/Core/Commands.cs
@@ -3622,15 +3622,15 @@ namespace NeptuneEvo.Core
             try
             {
                 if (!Group.CanUseCmd(player, "agm")) return;
-                if (!player.HasSharedData("AGM") || player.GetSharedData<bool>("AGM"))
+                if (!player.HasData("AGM") || !player.GetData<bool>("AGM"))
                 {
                     Trigger.ClientEvent(player, "AGM", true);
-                    player.SetSharedData("AGM", true);
+                    player.SetData("AGM", true);
                 }
                 else
                 {
                     Trigger.ClientEvent(player, "AGM", false);
-                    player.SetSharedData("AGM", false);
+                    player.SetData("AGM", false);
                 }
             }
             catch (Exception e) { Log.Write("EXCEPTION AT \"CMD\":\n" + e.ToString(), nLog.Type.Error); }
