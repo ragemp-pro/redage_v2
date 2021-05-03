9354d46b08f62ded6f36556c6769fe4b52fa6e22
user <user@localhost>
2021-05-04 02:36:52 +0300

    Фикс редкой ошибки в интерфейсе админ-панели (Мурзик).

--- server/client_packages/admin/adminpanel.js
+++ server/client_packages/admin/adminpanel.js
@@ -6,25 +6,21 @@ mp.keys.bind(0x77, false, function () {
 
 mp.events.add("openAdminPanel", (json, json2) => {
   if (!loggedin || chatActive || editing || cuffed) return;
+  
   global.adminPanel = mp.browsers.new('package://cef/AdminPanel/index.html');
+  global.adminPanel.active = false;
   global.menuOpen();
+
+  global.adminPanel.execute(`admlist.active=true`);
+  global.adminPanel.execute(`admlist.cmdlist=${json}`);
+  global.adminPanel.execute(`admlist.items=${json2}`);
+
   global.adminPanel.active = true;
-  setTimeout(function() {
-    global.adminPanel.execute(`admlist.active=true`);
-    global.adminPanel.execute(`admlist.cmdlist=${json}`);
-    global.adminPanel.execute(`admlist.items=${json2}`);
-  }, 250);
 });
 
 mp.events.add("closeAdminPanel", () => {
-  setTimeout(function() {
-		global.menuClose();
-		if(global.adminPanel)
-		{
-			global.adminPanel.active = false;
-			global.adminPanel.destroy();
-		}
-	}, 100);
+  global.menuClose();
+  if(global.adminPanel) global.adminPanel.destroy();
 });
 
 mp.events.add("getPlayerInfo", (id) => {
