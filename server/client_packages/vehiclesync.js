20fbc8b8661784b0a2e13f0c49f32e9cdada8c84
user <user@localhost>
2021-09-18 07:19:26 +0300

    Удалил лишний код.

--- server/client_packages/vehiclesync.js
+++ server/client_packages/vehiclesync.js
@@ -6,59 +6,6 @@ localplayer.setConfigFlag(241, true); // Отключаем глушения д�
 localplayer.setConfigFlag(429, true); // Отключаем попытку завести двигатель машины при входе в нее (PED_FLAG_STOP_ENGINE_TURNING)
 localplayer.setConfigFlag(35, false); // Отключаем автоматический шлем при посадке на мотоцикл (PED_FLAG_CAN_PUT_MOTORCYCLE_HELMET).
 
-
-/*
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freight"))) mp.game.streaming.requestModel(mp.game.joaat("freight"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freight"))) mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightcar"))) mp.game.streaming.requestModel(mp.game.joaat("freightcar"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightcar")))mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightgrain"))) mp.game.streaming.requestModel(mp.game.joaat("freightgrain"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightgrain"))) mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightcont1"))) mp.game.streaming.requestModel(mp.game.joaat("freightcont1"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightcont1"))) mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightcont2"))) mp.game.streaming.requestModel(mp.game.joaat("freightcont2"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freightcont2"))) mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freighttrailer"))) mp.game.streaming.requestModel(mp.game.joaat("freighttrailer"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("freighttrailer"))) mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("tankercar"))) mp.game.streaming.requestModel(mp.game.joaat("tankercar"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("tankercar"))) mp.game.wait(0);
-if(!mp.game.streaming.hasModelLoaded(mp.game.joaat("metrotrain"))) mp.game.streaming.requestModel(mp.game.joaat("metrotrain"));
-while(!mp.game.streaming.hasModelLoaded(mp.game.joaat("metrotrain"))) mp.game.wait(0);
-
-var train = mp.game.vehicle.createMissionTrain(15, 1907.842, -758.90493, 96.14539, true);
-var train2 = mp.game.vehicle.createMissionTrain(15, 1904.756, -758.90493, 96.14539, true);
-var train3 = mp.game.vehicle.createMissionTrain(24, 40.2,-1201.3,31.0, true);
-var train4 = mp.game.vehicle.createMissionTrain(24, -618.0,-1476.8,16.2, true);
-
-mp.game.invoke('0xAA0BC91BE0B796E3', train, 0.0);
-mp.game.invoke('0x16469284DB8C62B5', train, 0.0);
-mp.game.invoke('0xAA0BC91BE0B796E3', train2, 0.0);
-mp.game.invoke('0x16469284DB8C62B5', train2, 0.0);
-mp.game.invoke('0xAA0BC91BE0B796E3', train3, 0.0);
-mp.game.invoke('0x16469284DB8C62B5', train3, 0.0);
-mp.game.invoke('0xAA0BC91BE0B796E3', train4, 0.0);
-mp.game.invoke('0x16469284DB8C62B5', train4, 0.0);
-
-mp.events.add("Train_SetPosition", (type, x, y, z) => {
-    try {
-        switch(type) {
-			case 0:
-			mp.game.invoke('0x591CA673AA6AB736', train, x, y, z);
-			break;
-			case 1:
-			mp.game.invoke('0x591CA673AA6AB736', train2, x, y, z);
-			break;
-			case 2:
-			mp.game.invoke('0x591CA673AA6AB736', train3, x, y, z);
-			break;
-			case 3:
-			mp.game.invoke('0x591CA673AA6AB736', train4, x, y, z);
-			break;
-		}
-    } catch (e) { }
-});
-*/
-
 mp.events.add("VehStream_SetEngineStatus", (veh, status, lights, left, right) => {
     try {
         if (veh !== undefined) {
