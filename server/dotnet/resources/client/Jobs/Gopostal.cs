4087f81d1592930fc0e3b24ecd50427f5574b394
user <user@localhost>
2021-09-18 09:16:42 +0300

    Небольшой фикс работы почтальона (рабочая машина).

--- server/dotnet/resources/client/Jobs/Gopostal.cs
+++ server/dotnet/resources/client/Jobs/Gopostal.cs
@@ -205,7 +205,7 @@ namespace NeptuneEvo.Jobs
                 Notify.Send(player, NotifyType.Error, NotifyPosition.BottomCenter, $"Вы должны начать рабочий день", 3000);
                 return;
             }
-            if (player.GetData<Player>("WORK") != null)
+            if (player.GetData<Vehicle>("WORK") != null)
             {
                 NAPI.Entity.DeleteEntity(player.GetData<Vehicle>("WORK"));
                 player.SetData<Vehicle>("WORK", null);
